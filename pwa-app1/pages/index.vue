fe47acf3097a14fcf9f1a55dab9d58718d5dae2d
ChirathVINSIGHTURE <chirath.vandabona@insighture.com>
2025-02-13 13:56:28 +0530

    Revert PWA-APP 1

--- pwa-app1/pages/index.vue
+++ pwa-app1/pages/index.vue
@@ -26,7 +26,7 @@ onMounted(async () => {
 </template> -->
 
 
-<!-- <script setup lang="ts">
+<script setup lang="ts">
 const addData = async () => {
   if ('sharedStorage' in window) {
     await window.sharedStorage.set('sharedKey', 'Data from App 1');
@@ -41,36 +41,5 @@ const addData = async () => {
   <div>
     <h1>PWA - App 1</h1>
     <button @click="addData">Add Data to Shared Storage</button>
-  </div>
-</template> -->
-
-<script setup lang="ts">
-const getExperimentGroup = () => Math.round(Math.random());
-
-const injectContent = async () => {
-  if ("sharedStorage" in window) {
-    // Register the worklet
-    await window.sharedStorage.worklet.addModule("/ab-testing-worklet.js");
-
-    // Assign the user to a random group and store it in shared storage
-    await window.sharedStorage.set("ab-testing-group", getExperimentGroup(), {
-      ignoreIfPresent: true,
-    });
-
-    
-
-    console.log("User assigned to A/B testing group and data stored.");
-  } else {
-    console.log("Shared Storage API not supported.");
-  }
-};
-
-injectContent();
-</script>
-
-<template>
-  <div>
-    <h1>PWA - App 1</h1>
-    <p>User is being assigned to an A/B testing group...</p>
   </div>
 </template>
